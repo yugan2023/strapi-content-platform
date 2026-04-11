6bc78f9d95106bf4db21a2b504b2d2adcc7e0f6b
yugan2023 <yugan2023@gmail.com>
2026-04-12 03:58:12 +0800

    Update server.js

--- examples/getstarted/config/server.js
+++ examples/getstarted/config/server.js
@@ -5,6 +5,7 @@ const cronTasks = require('./src/cron-tasks');
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL'),
   cron: {
     enabled: true,
     tasks: cronTasks,
