c24a7d828d6d651b4ccfd075be566c7ed753f2a9
yugan2023 <yugan2023@gmail.com>
2026-04-12 12:37:43 +0800

    Update database.js

--- examples/getstarted/config/database.js
+++ examples/getstarted/config/database.js
@@ -1,50 +1,11 @@
-const sqlite = {
-  client: 'sqlite',
+module.exports = ({ env }) => ({
   connection: {
-    filename: '/tmp/data.db'  
-  },
-  useNullAsDefault: true,
-};
-
-const postgres = {
-  client: 'postgres',
+    client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
       ssl: {
         rejectUnauthorized: false,
       },
     },
-};
-
-const mysql = {
-  client: 'mysql',
-  connection: {
-    database: 'strapi',
-    user: 'strapi',
-    password: 'strapi',
-    port: 3306,
-    host: 'localhost',
   },
-};
-
-const mariadb = {
-  client: 'mysql',
-  connection: {
-    database: 'strapi',
-    user: 'strapi',
-    password: 'strapi',
-    port: 3307,
-    host: 'localhost',
-  },
-};
-
-const db = {
-  mysql,
-  sqlite,
-  postgres,
-  mariadb,
-};
-
-module.exports = {
-  connection: process.env.DB ? db[process.env.DB] || db.sqlite : db.sqlite,
-};
+});
