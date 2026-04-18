ac4dba625f2dddee6bc8d8f866d439dad1e1418a
yugan2023 <yugan2023@gmail.com>
2026-04-18 17:23:12 +0800

    Update plugins.js

--- examples/getstarted/config/plugins.js
+++ examples/getstarted/config/plugins.js
@@ -23,14 +23,14 @@ module.exports = ({ env }) => ({
   },
 
   // 新增：R2 / S3 上传配置
-  upload: {
+upload: {
   config: {
     provider: 'aws-s3',
 
     providerOptions: {
       accessKeyId: env('AWS_ACCESS_KEY_ID'),
       secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
-      endpoint: env('AWS_ENDPOINT'),
+      endpoint: env('AWS_ENDPOINT'), // ⭐ 只能用 cloudflarestorage
       region: 'auto',
       s3ForcePathStyle: true,
 
@@ -38,7 +38,9 @@ module.exports = ({ env }) => ({
         Bucket: env('AWS_BUCKET'),
       },
     },
-  baseUrl: env('CDN_URL'),
+
+    // ⭐ 用来改访问URL
+    baseUrl: env('CDN_URL'),
   },
 },
 });
