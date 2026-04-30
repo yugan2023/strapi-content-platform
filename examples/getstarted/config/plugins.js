a24758d692f7c3e70fbe3ae1cc9d564e3160b0cc
yugan2023 <yugan2023@gmail.com>
2026-05-01 00:40:25 +0800

    Update plugins.js

--- examples/getstarted/config/plugins.js
+++ examples/getstarted/config/plugins.js
@@ -23,24 +23,23 @@ module.exports = ({ env }) => ({
   },
 
   // 新增：R2 / S3 上传配置
-    upload: {
-        config: {
-            provider: 'strapi-provider-upload-cloudflare-r2',
-            providerOptions: {
-                accessKeyId: env('R2_ACCESS_KEY_ID'),
-                secretAccessKey: env('R2_ACCESS_SECRET'),
-                region: env('R2_REGION'),
-                params: {
-                    Bucket: env('R2_BUCKET'),
-                    accountId: env('R2_ACCOUNT_ID'),
-                    publicUrl: env('R2_PUBLIC_URL'),
-                },
+upload: {
+    config: {
+        provider: 'strapi-provider-cloudflare-r2',
+        providerOptions: {
+            accessKeyId: env('CF_ACCESS_KEY_ID'),
+            secretAccessKey: env('CF_ACCESS_SECRET'),
+            endpoint: env('CF_ENDPOINT'),
+            params: {
+                Bucket: env('CF_BUCKET'),
             },
-            actionOptions: {
-                upload: {},
-                uploadStream: {},
-                delete: {},
-            },
+            cloudflarePublicAccessUrl: env('CF_PUBLIC_ACCESS_URL'),
+        },
+        actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
         },
     },
+},
 });
