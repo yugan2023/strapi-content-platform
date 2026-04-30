3b23a7ff25fa447b24305b0472bb45c2f77a440d
yugan2023 <yugan2023@gmail.com>
2026-05-01 00:17:30 +0800

    Update plugins.js

--- examples/getstarted/config/plugins.js
+++ examples/getstarted/config/plugins.js
@@ -23,24 +23,24 @@ module.exports = ({ env }) => ({
   },
 
   // 新增：R2 / S3 上传配置
-upload: {
-  config: {
-    provider: 'aws-s3',
-
-    providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
-      endpoint: env('AWS_ENDPOINT'), // ⭐ 只能用 cloudflarestorage
-      region: 'auto',
-      s3ForcePathStyle: true,
-
-      params: {
-        Bucket: env('AWS_BUCKET'),
-      },
+    upload: {
+        config: {
+            provider: 'strapi-provider-upload-cloudflare-r2',
+            providerOptions: {
+                accessKeyId: env('R2_ACCESS_KEY_ID'),
+                secretAccessKey: env('R2_ACCESS_SECRET'),
+                region: env('R2_REGION'),
+                params: {
+                    Bucket: env('R2_BUCKET'),
+                    accountId: env('R2_ACCOUNT_ID'),
+                    publicUrl: env('R2_PUBLIC_URL'),
+                },
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
+        },
     },
-
-    // ⭐ 用来改访问URL
-    baseUrl: env('CDN_URL'),
-  },
-},
 });
